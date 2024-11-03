e5b65289b58a38bb8f3268e1ac3e266b11de1523
FlamingHerb <msbbomisol@gmail.com>
2024-11-03 16:39:36 +0800

    feat: working camera

---
+++ scripts/game_main/camera_2d.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+const dead_zone: int = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		position = event.position
