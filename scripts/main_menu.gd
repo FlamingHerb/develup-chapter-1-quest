d426234210ca5d9f47c85917f006953a0b4c846e
FlamingHerb <msbbomisol@gmail.com>
2024-11-07 12:53:13 +0800

    feat: credits panel added

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,18 @@
 extends Node
 
+@onready var anim_player = $AnimationPlayer
+@onready var current_scene: String = ""
+
 @export var game_scene: PackedScene
+
+var transition_sfx = [
+	"res://audio/sfx/transition_swing_001.mp3",
+	"res://audio/sfx/transition_swing_002.mp3",
+	"res://audio/sfx/transition_swing_003.mp3",
+	"res://audio/sfx/transition_swing_004.mp3"
+]
+
+
 
 #===============================================================================
 # Engine Signature/Signals functions
@@ -23,9 +35,17 @@ func _on_start_game_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
 	
 	
-
 func _on_how_to_play_pressed() -> void:
 	pass # Replace with function body.
+
+func _on_credits_pressed() -> void:
+	AudioManager.sfx_play(transition_sfx.pick_random())
+	if current_scene == "credits":
+		current_scene = ""
+		anim_player.play("hide_credits")
+	else:
+		current_scene = "credits"
+		anim_player.play("show_credits")
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
